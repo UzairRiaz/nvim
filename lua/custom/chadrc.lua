fe44a7490627bdc520680b36ecdaf14537e06cab
Uzair Riaz <uzairriaz.pk@gmail.com>
2023-10-04 17:29:45 +0500

    transparant background

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "tokyonight",
-  theme_toggle = { "tokyonight", "one_light" },
+  theme = "aquarium",
+  theme_toggle = { "aquarium", "one_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
